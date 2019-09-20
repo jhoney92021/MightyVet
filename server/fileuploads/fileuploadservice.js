b75534c4e79c5cf44144a5a5b33add858e65eac6
jhoney92021 <51093828+jhoney92021@users.noreply.github.com>
2019-09-19 18:41:40 -0700

    starting on office/resource templates

--- server/fileuploads/fileuploadservice.js
+++ server/fileuploads/fileuploadservice.js
@@ -1,14 +1,14 @@
-const aws = require('aws-sdk');
+// const aws = require('aws-sdk');
 
 aws.config.update({
     // Your SECRET ACCESS KEY from AWS should go here,
     // Never share it!
     // Setup Env Variable, e.g: process.env.SECRET_ACCESS_KEY
-    secretAccessKey: require('../secrets').aws_key,
+    // secretAccessKey: require('../secrets').aws_key,
     // Not working key, Your ACCESS KEY ID from AWS should go here,
     // Never share it!
     // Setup Env Variable, e.g: process.env.ACCESS_KEY_ID
-    accessKeyId: require('../secrets').aws_keyid,
+    // accessKeyId: require('../secrets').aws_keyid,
     region: 'us-west-1' // region of your bucket
 });
 
